f23e6fe88b467887d1732a73dd28b618ce5a23f3
pentem <martijn.pentenga@movares.nl>
2022-11-06 22:00:20 +0100

    runs-on windows-latest to pipline

--- tests/test_acad_locally.py
+++ tests/test_acad_locally.py
@@ -1,5 +1,5 @@
-# from shapely.geometry import LineString, Point
-# from shapelyAcad import ShapelyAcad
+from shapely.geometry import LineString, Point
+from shapelyAcad import ShapelyAcad
 
 
 class TestSimplePolyline:
@@ -9,18 +9,18 @@ class TestSimplePolyline:
         # All local needs interaction with autocad
         #####
 
-        # acad = ShapelyAcad()
-        #
-        # # draw point in AutoCAD
-        # point = Point([6, 3])
-        # acad.draw_shapely(point)
-        # acad.draw_text("Sample Text", point)
-        #
-        # # draw line in AutoCAD
-        # # line = LineString([[3, 0], [3, 10], [3, 20], [3, 30]])
-        # line = LineString([[3, 0, 0], [3, 10, 0], [3, 20, 0], [3, 30, 0]])
-        # acad.draw_shapely(line, color=3)
-        # acad.draw_profile(line, 5, 10)
-        #
-        # # # select objects from AutoCAD
-        # # list_of_shapley_objects = acad.get_shapelies_from_selection()
+        acad = ShapelyAcad()
+
+        # draw point in AutoCAD
+        point = Point([6, 3])
+        acad.draw_shapely(point)
+        acad.draw_text("Sample Text", point)
+
+        # draw line in AutoCAD
+        # line = LineString([[3, 0], [3, 10], [3, 20], [3, 30]])
+        line = LineString([[3, 0, 0], [3, 10, 0], [3, 20, 0], [3, 30, 0]])
+        acad.draw_shapely(line, color=3)
+        acad.draw_profile(line, 5, 10)
+
+        # # select objects from AutoCAD
+        # list_of_shapley_objects = acad.get_shapelies_from_selection()
