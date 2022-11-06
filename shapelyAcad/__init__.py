7ce26b144f9a69ab44b0777e8bd093794ca8205f
pentem <martijn.pentenga@movares.nl>
2022-11-06 22:18:04 +0100

    refactor

--- shapelyAcad/__init__.py
+++ shapelyAcad/__init__.py
@@ -2,6 +2,6 @@ from typing import Any, List
 
 __version__ = "0.0.1-dev5"
 
-from shapelyAcad.autocad import ShapelyAcad
+from shapelyAcad.core import ShapelyAcad
 
 __all__: List[Any] = ["ShapelyAcad"]
